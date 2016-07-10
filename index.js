1dc16e552fc51f9847db984d6eae45e2478973d0
Jon Gold <hello@designedbygold.com>
2016-07-11 00:23:21 +0100

    set up linting

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 const { __, assoc, compose, drop, head, isEmpty, map, reduce, reject, replace,
-  split, tail, trim } = require('ramda')
-const commands = require('./commands')
+  split, tail, trim } = require('ramda');
+const commands = require('./src/commands');
 
 // actualVimInfo :: _ -> path
 const actualVimInfo = () => {
-  const HOME = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE
-  return path.join(HOME, '.viminfo')
-}
+  const HOME = process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE;
+  return path.join(HOME, '.viminfo');
+};
 
 // impure
 // resolveInfoPath :: [args] -> path
 const resolveInfoPath = (args) => {
-  const a = args[2]
+  const a = args[2];
   if (a) {
-    return path.resolve(a)
+    return path.resolve(a);
   }
-  return actualVimInfo()
-}
+  return actualVimInfo();
+};
 
 // removeStr :: str -> str -> str
-const removeStr = replace(__, '')
+const removeStr = replace(__, '');
 
 // splitAtHeaders :: str -> [str]
-const splitAtHeaders = split('# ')
+const splitAtHeaders = split('# ');
 
 // rejectEmpty :: [a] -> [a]
-const rejectEmpty = reject(isEmpty)
+const rejectEmpty = reject(isEmpty);
 
 // splitLines :: str -> [str]
-const splitLines = split('\n')
+const splitLines = split('\n');
 
 // cleanTitle :: str -> str
 const cleanTitle = compose(
@@ -38,19 +39,19 @@ const cleanTitle = compose(
   removeStr(/:/g),
   removeStr(/\(newest first\)/g),
   removeStr(/\(newest to oldest\)/g)
-)
+);
 
 // title :: [str] -> str
-const title = compose(cleanTitle, head)
+const title = compose(cleanTitle, head);
 
 // items :: [str] -> [str]
-const items = tail
+const items = tail;
 
 // intoObject :: {k: v} -> [str] -> {k: v}
-const intoObject = (acc, val) => assoc(title(val), items(val), acc)
+const intoObject = (acc, val) => assoc(title(val), items(val), acc);
 
 // removeIntro :: [str] -> [str]
-const removeIntro = drop(2)
+const removeIntro = drop(2);
 
 // processVimInfo :: str -> {k: v}
 const processVimInfo = compose(
@@ -60,10 +61,10 @@ const processVimInfo = compose(
   rejectEmpty,
   map(compose(rejectEmpty, splitLines)),
   splitAtHeaders
-)
+);
 
 fs.readFile(resolveInfoPath(process.argv), { encoding: 'utf-8' }, (err, res) => {
-  if (err) { throw err }
-  const info = processVimInfo(res)
-  console.log(commands(info['Command Line History']))
-})
+  if (err) { throw err; }
+  const info = processVimInfo(res);
+  console.log(commands(info['Command Line History']));
+});
---
+++ src/commands.js
@@ -0,0 +1,37 @@
+const { append, compose, countBy, filter, head, identity, map, match, prop,
+  reverse, sortBy, take, test, toPairs } = require('ramda');
+
+// saveAndQuit :: Regex
+const saveAndQuit = /:wq/;
+
+// getCommand :: str -> str
+const getCommand = compose(head, match(/:(%s|\S+)/g));
+
+const analyzeCommands = commands => {
+  // this is gross
+  let suggestions = [];
+  const saves = filter(test(saveAndQuit), commands);
+  if (saves) {
+    suggestions = append({
+      message: `you used \`:wq\` ${saves.length} times, try \`ZZ\``,
+    }, suggestions);
+  }
+  const commandHeads = map(getCommand);
+  const mostCommon = compose(
+    take(10),
+    reverse,
+    sortBy(prop(1)),
+    toPairs,
+    countBy(identity),
+    commandHeads
+  );
+  const mc = mostCommon(commands);
+  const hhead = compose(head, head);
+  suggestions = append({
+    message: `Your most common command is \`${hhead(mc)}\``,
+  }, suggestions);
+
+  return suggestions;
+};
+
+module.exports = analyzeCommands;
